4e46e6ba2c3a00c11b1f0bab342dc05cb8d3cb1e
Radim Hejduk <radim@radimhejduk.cz>
2021-07-19 21:22:19 +0200

    election admin continuous fetch

--- src/views/Admin/Election.vue
+++ src/views/Admin/Election.vue
@@ -3,29 +3,35 @@
     <div v-if="!loading">
       <Header :title="title" />
       
-      <div class="px-4">
-        <div class="grid grid-cols-2">
+      <div class="px-8 md:px-12">
+        <div class="grid md:grid-cols-2">
           <div>
             <div class="mb-12">
-              <h3 class="text-center text-xl">Možnosti volby</h3>
-              <ul>
+              <h3 class="text-xl pb-4">Možnosti volby</h3>
+              <ul class="list-disc list-inside">
                 <li v-for="option in Object.entries(admin.item.options)" :key="option[0]">
                   {{ option[1].title }}
                 </li>
               </ul>
             </div>
-            <div>
-              <h3 class="text-center text-xl">Administrace volby</h3>
+            <div class="mb-12">
+              <h3 class="text-xl pb-4">Administrace volby</h3>
               <p>
-                Email adminstrátora: {{admin.item.admin}}
+                Email administrátora: {{admin.item.admin}}
               </p>
             </div>
           </div>
           <div>
-            <h3 class="text-center text-xl">Hlasující</h3>
+            <h3 class="text-xl pb-4">Hlasující</h3>
             <ul>
               <li v-for="voter in admin.item.voters" :key="voter">
-                {{ voter.voted }}
+                <span v-if="voter.voted === false" class="text-gray-400 pr-4 text-lg">
+                  ⦾
+                </span>
+
+                <span v-else class="text-green-400 pr-4 text-lg">
+                  ⦿
+                </span>
                 {{ voter.voter.address }}
               </li>
             </ul>
@@ -66,6 +72,10 @@ export default {
       .dispatch('admin/fetch', { slug: this.$route.params.slug, token: this.$route.params.token });
 
     this.loading = false;
+
+    setInterval(() => {
+      this.$store.dispatch('admin/fetch', { slug: this.admin.item.slug, token: this.admin.item.token });
+    }, 1000);
   }
 }
 </script>
